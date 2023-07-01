2080fc9a30fa56c862ae9ac61baadcc81b8a3ab7
amansingh2 <aman.nitmizoram@gmail.com>
2023-07-01 23:45:21 +0530

    added files for Library Management System

---
+++ Library Management System/src/BookLending.java
@@ -0,0 +1,11 @@
+import java.time.LocalDateTime;
+
+class BookLending {
+
+    static LocalDateTime creationDate;
+    static LocalDateTime dueDate;
+    // private Date returnDate;
+    private static String boodId;
+    private static String memberId;
+
+}
---
+++ Library Management System/src/Fine.java
@@ -0,0 +1,12 @@
+import java.util.Date;
+
+class Fine {
+    private Date creationDate;
+    private String bookId;
+    private String memberId;
+    private int finePerDay = 1;
+
+    public int calFine(int days) {
+        return days * finePerDay;
+    }
+}
