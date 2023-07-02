cf15011c3220e99fee0ddc01759009045cf5580f
amansingh2 <aman.nitmizoram@gmail.com>
2023-07-02 22:54:45 +0530

    Chess's - LLD

--- Facebook/src/App.java
+++ Facebook/src/App.java
@@ -2,7 +2,9 @@ class App
 {
     public static void main(String[] args)
     {
-
-
+        public void resetPassword(String password)
+        {
+            this.password = password;
+        }
     }
 }
